e4add965d26f3bace4f3ddf5c65103967a13e27a
Jacky Sihalard <sihalardjacky@gmail.com>
2025-06-19 13:04:19 +0700

    #

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-//import "./globals.css";
+import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
