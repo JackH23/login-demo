61eeb41a69cd67d74185cdd030438c4c176c6bae
JackH23 <136872545+JackH23@users.noreply.github.com>
2025-06-25 13:56:05 +0700

    Add friend feature

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,6 +4,9 @@ import User from '@/models/User';
 
 export async function GET() {
   await dbConnect();
-  const users = await User.find({}, 'username position age image -_id').lean();
+  const users = await User.find(
+    {},
+    'username position age image friends -_id'
+  ).lean();
   return NextResponse.json({ users });
 }
